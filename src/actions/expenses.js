205f00f32fd0bb5c8e03ef1869bfbc1ccab0c08f
Will <will.macedo@hotmail.com>
2021-11-25 02:05:31 -0500

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,6 +33,14 @@ export const removeExpense = ({ id } = {}) => ({
     id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
+    };
+};
+
 // EDIT_EXPENSE
 
 export const editExpense = (id, updates) => ({
